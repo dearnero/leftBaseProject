2d81f80a2ba9c53bead7056d79232d0640a3174f
huangfu <wx172296059@126.com>
2024-05-31 09:42:15 +0800

    change mergeSortedArrays function V3

--- src/main/java/com/hf/left/algorithms/MergeSortedArrays.java
+++ src/main/java/com/hf/left/algorithms/MergeSortedArrays.java
@@ -11,21 +11,26 @@ public class MergeSortedArrays {
     public static void merge(int[] num1, int m, int[] num2, int n){
         int i = m - 1;
         int j = n - 1;
-        for(int k = m + n - 1;k>=0;--k){
-            if(j < 0 || (i >= 0 && num1[i] >= num2[j])){
-                num1[k] = num1[i];
-                i--;
-            }else{
-                num1[k] = num2[j];
-                j--;
+        int tail = m + n - 1;
+        int cur = 0;
+        while (i >= 0 || j >=0){
+            if (i == -1){
+                cur = num2[j--];
+            }else if (j == -1){
+                cur = num1[i--];
+            }else if (num1[i] >= num2[j]){
+                cur = num1[i--];
+            }else {
+                cur = num2[j--];
             }
+            num1[tail--] = cur;
         }
     }
 
     public static void main(String[] args) {
-        int[] num1 = {2,0};
+        int[] num1 = {0};
         int[] num2 = {1};
-        merge(num1, 1, num2,1);
+        merge(num1, 0, num2,1);
         for (int i : num1) {
             System.out.println(i);
         }
