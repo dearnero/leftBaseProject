065eb0f911384bc189b6985f3766b6d52b5b58a6
huangfu <wx172296059@126.com>
2024-06-03 09:29:06 +0800

    new isAnagram

---
+++ src/main/java/com/hf/left/algorithms/HashTest.java
@@ -0,0 +1,28 @@
+package com.hf.left.algorithms;
+
+/**
+ * @description: TODO
+ * @author: huang fu
+ * @date: 2024/6/3 09:19
+ * @version: 1.0
+ */
+public class HashTest {
+
+    public boolean isAnagram(String s, String t){
+        if (s.length() != t.length()){
+            return false;
+        }
+        int[] counts = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counts[s.charAt(i) - 'a']++;
+        }
+        for (int j = 0; j < t.length(); j++) {
+            counts[t.charAt(j) - 'a']--;
+            if (counts[t.charAt(j) - 'a'] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
--- src/main/java/com/hf/left/algorithms/ReverseLinkedList.java
+++ src/main/java/com/hf/left/algorithms/ReverseLinkedList.java
@@ -7,21 +7,45 @@ import com.hf.left.algorithms.definition.ListNode;
  */
 public class ReverseLinkedList {
 
+    public ListNode addTwoNumbers(ListNode head1, ListNode head2){
+        head1 = reverseList(head1);
+        head2 = reverseList(head2);
+        ListNode addList = addReversed(head1, head2);
+        return reverseList(addList);
+    }
+
+    public ListNode addReversed(ListNode head1, ListNode head2){
+        ListNode dummy = new ListNode(0);
+        ListNode sumNode = dummy;
+        int carry = 0;
+        while (head1 != null || head2 != null){
+            int sum = (head1 == null ? 0 : head1.val) + (head2 == null ? 0 : head2.val);
+            carry = sum > 10 ? 1 : 0;
+            int newSumVal = sum > 10 ? sum - 10 : sum;
+            sumNode.next = new ListNode(newSumVal);
+            sumNode = sumNode.next;
+            head1 = head1 != null ? head1.next : null;
+            head2 = head2 != null ? head2.next : null;
+        }
+        if (carry > 0){
+            sumNode.next = new ListNode(1);
+        }
+        return dummy;
+    }
+
     public ListNode reverseList(ListNode head) {
+        ListNode cur = head;
         ListNode prev = null;
-        ListNode current = head;
-        while(current != null) {
-            ListNode next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
+        while (cur.next != null){
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
         }
         return prev;
     }
 
     public static void main(String[] args) {
         ListNode tem1 = new ListNode(1);
-
-
     }
 }
