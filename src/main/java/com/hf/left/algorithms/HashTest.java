2d01fe8d2de844589f200656f2cd913bc59ba685
huangfu <wx172296059@126.com>
2024-06-03 10:46:45 +0800

    new isAlienSorted

--- src/main/java/com/hf/left/algorithms/HashTest.java
+++ src/main/java/com/hf/left/algorithms/HashTest.java
@@ -8,6 +8,10 @@ package com.hf.left.algorithms;
  */
 public class HashTest {
 
+    /**
+     * 是否是异位词
+     * @return
+     */
     public boolean isAnagram(String s, String t){
         if (s.length() != t.length()){
             return false;
@@ -23,5 +27,39 @@ public class HashTest {
             }
         }
         return true;
+    }
+
+    /**
+     * 外星语言是否排序
+     * @return
+     */
+    public boolean isAlienSorted(String[] words, String order){
+
+        int[] dict = new int[26];
+        for (int i = 0; i < order.length(); i++) {
+            dict[order.charAt(i) - 'a'] = i;
+        }
+        for (int i = 1; i < words.length; i++) {
+            if (!isSort(words[i], words[i - 1], dict)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isSort(String t1, String t2, int[] dirt){
+        int minLength = Math.min(t1.length(), t2.length());
+        for (int i = 0; i < minLength; i++) {
+            if (t1.charAt(i) == t2.charAt(i)){
+                continue;
+            }
+            return dirt[t1.charAt(i) - 'a'] > dirt[t2.charAt(i) - 'a'];
+        }
+        return t1.length() >= t2.length();
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"ubg","kwh"};
+        new HashTest().isAlienSorted(words, "qcipyamwvdjtesbghlorufnkzx");
     }
 }
