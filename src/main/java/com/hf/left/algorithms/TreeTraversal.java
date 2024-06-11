75b07347e4bda6c599f8b0725c042d3837207c86
huangfu <wx172296059@126.com>
2024-06-11 09:35:33 +0800

    findBottomLeftValue、largestValues2

--- src/main/java/com/hf/left/algorithms/TreeTraversal.java
+++ src/main/java/com/hf/left/algorithms/TreeTraversal.java
@@ -1,6 +1,8 @@
 package com.hf.left.algorithms;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -72,22 +74,110 @@ public class TreeTraversal {
         }
 
         void leverOrder(){
-            if (root != null){
-                return;
-            }
             Queue<Node> queue = new LinkedList<>();
             queue.add(root);
             while (!queue.isEmpty()){
-                Node current = queue.poll();
-                System.out.println(current.value + " ");
-                if (current.left != null){
-                    queue.add(current.left);
+                Node poll = queue.poll();
+                if (poll.left != null){
+                    queue.add(poll.left);
+
                 }
-                if (current.right != null){
-                    queue.add(current.right);
+                if (poll.right != null){
+                    queue.add(poll.right);
                 }
             }
         }
+    }
+
+    /**
+     * 二叉树中每层的最大值
+     */
+    public List<Integer> largestValues(Node root){
+        int current = 0;
+        int next = 0;
+        Queue<Node> queue = new LinkedList<>();
+        if (root != null){
+            queue.offer(root);
+            current = 1;
+        }
+        List<Integer> result = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        while (!queue.isEmpty()){
+            Node node = queue.poll();
+            current--;
+            max = Math.max(max, node.value);
+            if (node.left !=null){
+                queue.offer(node.left);
+                next++;
+            }
+            if (node.right != null){
+                queue.offer(node.right);
+                next++;
+            }
+            if (current == 0){
+                result.add(max);
+                max = Integer.MIN_VALUE;
+                current = next;
+                next = 0;
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> largestValues2(Node root) {
+
+        Queue<Node> queue1 = new LinkedList<>();
+        if (root != null){
+            queue1.offer(root);
+        }
+        List<Integer> result = new ArrayList<>();
+        Queue<Node> queue2 = new LinkedList<>();
+        int max = Integer.MIN_VALUE;
+        while (!queue1.isEmpty()){
+            Node node = queue1.poll();
+            max = Math.max(max, node.value);
+            if (node.left != null){
+                queue2.add(node.left);
+            }
+            if (node.right != null){
+                queue2.add(node.right);
+            }
+            if (queue1.isEmpty()){
+                result.add(max);
+                max = Integer.MIN_VALUE;
+                queue1 = queue2;
+                queue2 = new LinkedList<>();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 二叉树最低层最左边的值
+     */
+    public int findBottomLeftValue(Node node){
+        Queue<Node> queue1 = new LinkedList<>();
+        queue1.offer(node);
+        int leftValue = node.value;
+        Queue<Node> queue2 = new LinkedList<>();
+        while (!queue1.isEmpty()){
+            Node poll = queue1.poll();
+            if (poll.left != null){
+                queue2.offer(poll.left);
+            }
+            if (poll.right != null){
+                queue2.offer(poll.right);
+            }
+            if (queue1.isEmpty()){
+                queue1 = queue2;
+                if (!queue2.isEmpty()){
+                    Node peek = queue2.peek();
+                    leftValue = peek.value;
+                }
+                queue2 = new LinkedList<>();
+            }
+        }
+        return leftValue;
     }
 
     public static void main(String[] args) {
