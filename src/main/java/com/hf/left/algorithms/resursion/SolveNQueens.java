28a47c6cfec445913e7f753ba0e53f990cf1c691
huangfu <wx172296059@126.com>
2024-07-03 22:54:44 +0800

    solverNqueens

--- src/main/java/com/hf/left/algorithms/resursion/SolveNQueens.java
+++ src/main/java/com/hf/left/algorithms/resursion/SolveNQueens.java
@@ -11,55 +11,40 @@ import java.util.*;
 public class SolveNQueens {
 
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> solutions = new ArrayList<>();
-        int[] queens = new int[n];
-        Arrays.fill(queens, -1);
-        Set<Integer> columns = new HashSet<>();
-        Set<Integer> diagonals1 = new HashSet<>();
-        Set<Integer> diagonals2 = new HashSet<>();
-        backTrack(solutions,queens, n,0, columns,diagonals1,diagonals2);
-        return solutions;
+
+        List<List<String>> ans = new ArrayList<>();
+        int[] cols = new int[n];
+        boolean[] usedCols = new boolean[n];
+        boolean[] ding1 = new boolean[n * 2 - 1];
+        boolean[] ding2 = new boolean[n * 2 - 1];
+        helper(0, n, cols, usedCols, ding1, ding2, ans);
+        return ans;
     }
 
-    public void backTrack(List<List<String>> solutions, int[] queens , int n, int row, Set<Integer> columns,
-                          Set<Integer> diagonals1, Set<Integer> diagonals2){
+    private void helper(int row, int n, int[] cols, boolean[] usedCols, boolean[] ding1, boolean[] ding2, List<List<String>> ans){
         if (row == n){
-            List<String> board = generateBoard(queens, n);
-            solutions.add(board);
-        }else {
-            for (int i = 0; i < n; i++) {
-                if (columns.contains(i)){
-                    continue;
-                }
-                int diagonal1 = row - i;
-                if (diagonals1.contains(diagonal1)){
-                    continue;
-                }
-                int diagonal2 = row + i;
-                if (diagonals2.contains(diagonal2)){
-                    continue;
-                }
-                queens[row] = i;
-                columns.add(i);
-                diagonals1.add(diagonal1);
-                diagonals2.add(diagonal2);
-                backTrack(solutions, queens, n, row + 1, columns, diagonals1, diagonals2);
-                queens[row] = -1;
-                columns.remove(i);
-                diagonals1.remove(diagonal1);
-                diagonals2.remove(diagonal2);
+            List<String> oneStrList = new ArrayList<>();
+            for (int i = 0; i < cols.length; i++) {
+                char[] kk = new char[n];
+                Arrays.fill(kk, '.');
+                kk[cols[i]] = 'Q';
+                oneStrList.add(new String(kk));
+            }
+            ans.add(oneStrList);
+        }
+        for (int c = 0; c < n; c++) {
+            int ding2Index = row - c + n - 1;
+            if (!usedCols[c] && !ding1[row + c] && !ding2[ding2Index]){
+                cols[row] = c;
+                usedCols[c] = ding1[row + c] = ding2[ding2Index] = true;
+                helper(row + 1, n, cols, usedCols, ding1, ding2, ans);
+                usedCols[c] = ding1[row + c] = ding2[ding2Index] = false;
             }
         }
     }
 
-    private List<String> generateBoard(int[] queens, int n) {
-        List<String> board = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            char[] row = new char[n];
-            Arrays.fill(row, '.');
-            row[queens[i]] = '0';
-            board.add(new String(row));
-        }
-        return board;
+    public static void main(String[] args) {
+        System.out.println(new SolveNQueens().solveNQueens(4));
     }
+
 }
