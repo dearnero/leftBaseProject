c5953af865ccf26474a8e192bdbcfde5d47ee258
huangfu <wx172296059@126.com>
2024-07-04 09:33:27 +0800

    solverNqueens

--- src/main/java/com/hf/left/algorithms/resursion/SolveNQueens.java
+++ src/main/java/com/hf/left/algorithms/resursion/SolveNQueens.java
@@ -11,40 +11,42 @@ import java.util.*;
 public class SolveNQueens {
 
     public List<List<String>> solveNQueens(int n) {
-
         List<List<String>> ans = new ArrayList<>();
         int[] cols = new int[n];
         boolean[] usedCols = new boolean[n];
-        boolean[] ding1 = new boolean[n * 2 - 1];
-        boolean[] ding2 = new boolean[n * 2 - 1];
+        boolean[] ding1 = new boolean[2 * n - 1];
+        boolean[] ding2 = new boolean[2 * n - 1];
         helper(0, n, cols, usedCols, ding1, ding2, ans);
         return ans;
     }
 
-    private void helper(int row, int n, int[] cols, boolean[] usedCols, boolean[] ding1, boolean[] ding2, List<List<String>> ans){
-        if (row == n){
-            List<String> oneStrList = new ArrayList<>();
+    private void helper(int r, int n, int[] cols, boolean[] usedCols, boolean[] ding1, boolean[] ding2, List<List<String>> ans) {
+
+        // 终止条件
+        if(r == n){
+            List<String> oneAns = new ArrayList<>();
             for (int i = 0; i < cols.length; i++) {
-                char[] kk = new char[n];
-                Arrays.fill(kk, '.');
-                kk[cols[i]] = 'Q';
-                oneStrList.add(new String(kk));
+                char[] chars = new char[n];
+                Arrays.fill(chars, '.');
+                chars[cols[i]] = 'Q';
+                oneAns.add(new String(chars));
             }
-            ans.add(oneStrList);
+            ans.add(oneAns);
         }
         for (int c = 0; c < n; c++) {
-            int ding2Index = row - c + n - 1;
-            if (!usedCols[c] && !ding1[row + c] && !ding2[ding2Index]){
-                cols[row] = c;
-                usedCols[c] = ding1[row + c] = ding2[ding2Index] = true;
-                helper(row + 1, n, cols, usedCols, ding1, ding2, ans);
-                usedCols[c] = ding1[row + c] = ding2[ding2Index] = false;
+            int rc = r - c + n - 1;
+            if (!usedCols[c] && !ding1[r + c] && !ding2[rc]){
+                cols[r] = c;
+                usedCols[c] = ding1[r + c] = ding2[rc] = true;
+                helper(r + 1, n, cols, usedCols, ding1, ding2, ans);
+                usedCols[c] = ding1[r + c] = ding2[rc] = false;
             }
         }
     }
 
+
     public static void main(String[] args) {
-        System.out.println(new SolveNQueens().solveNQueens(4));
+        System.out.println(new SolveNQueens().solveNQueens(8));
     }
 
 }
