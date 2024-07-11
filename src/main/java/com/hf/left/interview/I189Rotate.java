51962f5fe34b6d9047d6822660daa9d9550dd550
huangfu <wx172296059@126.com>
2024-07-11 22:23:50 +0800

    I189Rotate

---
+++ src/main/java/com/hf/left/interview/I189Rotate.java
@@ -0,0 +1,30 @@
+package com.hf.left.interview;
+
+/**
+ * @description: TODO
+ * @author: huang fu
+ * @date: 2024/7/11 21:53
+ * @version: 1.0
+ */
+public class I189Rotate {
+
+    public void rotate(int[] nums, int k) {
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end){
+        while (start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        new I189Rotate().rotate(new int[]{-1,-100,3,99}, 2);
+    }
+}
